1e622dda2d5f25ba50a925cb73a01d66808af775
voson wang <voson@ricnsmart.com>
2019-04-24 09:38:56 +0800

    fix(regexp)：纠正一个手误

--- util/regexp.go
+++ util/regexp.go
@@ -6,7 +6,7 @@ var (
 	// 域名，只匹配数字、字母和.
 	OnlyDomainRecord = regexp.MustCompile(`^(\w|\.)+$`)
 
-	HasDomainRecord = regexp.MustCompile(`[0-9]+`)
+	HasDomainRecord = regexp.MustCompile(`[0-9a-z\\.]+`)
 
 	HasNum = regexp.MustCompile(`[0-9]+`)
 )
