7720cf817f356d161578b4710d5595f4ef572f71
voson@ricnsmart.com <voson@2018>
2019-05-18 23:24:29 +0800

    refactor(regexp): 增加手机号匹配

--- util/regexp.go
+++ util/regexp.go
@@ -6,6 +6,9 @@ var (
 	// 域名，只匹配数字、字母和.
 	OnlyDomainRecord = regexp.MustCompile(`^(\w|\.)+$`)
 
+	// 匹配11位手机
+	OnlyMobile = regexp.MustCompile("^1\\d{10}$")
+
 	HasDomainRecord = regexp.MustCompile(`[0-9a-z\\.]+`)
 
 	HasNum = regexp.MustCompile(`[0-9]+`)
